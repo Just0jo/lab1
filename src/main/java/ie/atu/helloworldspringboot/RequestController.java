591a8fe7197cf7341bb68dc2546092c20fffec89
G00407342@atu.ie <G00407342@atu.ie>
2023-10-03 16:58:47 +0100

    start

---
+++ src/main/java/ie/atu/helloworldspringboot/RequestController.java
@@ -0,0 +1,17 @@
+package ie.atu.helloworldspringboot;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+@RestController
+public class RequestController {
+    @GetMapping("greet/{name}")
+    public String storycuz(@PathVariable String name) {
+        return "Hello," + name;
+    }
+    @GetMapping("/details")
+    public String storycuz(@RequestParam String name , int age) {
+        return "Name : age," + age + name;
+    }
+}
