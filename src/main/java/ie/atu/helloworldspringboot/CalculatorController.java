709d642b40084ee761ef09082678fd5d7c2417e1
Just0jo <114908443+Just0jo@users.noreply.github.com>
2023-10-09 22:50:31 +0100

    CALCULARTOR AND SOLUTION CLASS HW

--- src/main/java/ie/atu/helloworldspringboot/CalculatorController.java
+++ src/main/java/ie/atu/helloworldspringboot/CalculatorController.java
@@ -1,4 +1,30 @@
 package ie.atu.helloworldspringboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CalculatorController {
-}
+    @GetMapping("/calculate /{numb1}/{numb2}/(operation}")
+    public Solution calculate(@PathVariable int numb1, @PathVariable int numb2, @PathVariable String operation){
+        if (operation == "add") {
+            return new Solution(operation, numb1 + numb2);
+        }
+        if (operation == "subtract") {
+            return new Solution(operation, numb1 - numb2);
+        }
+        if (operation == "multiply") {
+            return new Solution(operation, numb1 * numb2);
+        }
+        if (operation == "divide") {
+            return new Solution(operation, numb1 / numb2);
+        }
+        return new Solution("",0);
+
+        }
+    }
+
+
+
+
---
+++ src/main/java/ie/atu/helloworldspringboot/Solution.java
@@ -0,0 +1,14 @@
+package ie.atu.helloworldspringboot;
+
+public class Solution {
+    String operation = "operation";
+    String getOperation = "";
+    String total = "total";
+    int answer = 0;
+    public Solution(String operation, int answer){
+        getOperation = operation;
+        this.answer = answer;
+
+    }
+
+}
